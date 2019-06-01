ecefe9a08269a3be7deb0e52abfefac2d07aff2c
Pasan Godamune <pasanjg@gmail.com>
2019-06-01 18:07:59 +0530

    minor fixes

--- src/App.js
+++ src/App.js
@@ -1,43 +1,39 @@
 import React from 'react';
-import {  Route, Router , BrowserRouter, Redirect, IndexRoute} from 'react-router-dom';
+import { Route, BrowserRouter } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import logo from './logo.svg';
 
 import Home from './components/Home';
 import Header from './components/Header';
 import Signin from './components/signin';
 
 import './App.css';
-import {Nav, Navbar, NavDropdown, MenuItem, Tabs, ButtonToolbar, 
-        Button, Table, ButtonGroup, Row, Col, Grid, Panel, FormGroup , FormControl} from 'react-bootstrap';
+
+export class App extends React.Component {
+
+  // constructor() {
+  //   super();
+  // }
 
 
-export class App extends React.Component{
-
-  constructor(){
-    super();
-  }
-
-
-  render(){
-  return (
-    <div>
-      <BrowserRouter>
-        <div>
-          {/* <Route path="/" 
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <div>
+            {/* <Route path="/" 
           render={(routeProps) => (
             <Header {...routeProps} user = {"user"} />
             )}/> */}
-          <Route path="/" component= {Header}/>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/home' component={Home} />
-          <Route path='/login' component={Signin} />
-        </div>  
-      </BrowserRouter>
-  </div>
-  );
-}
+            <Route path="/" component={Header} />
+            <Route exact path='/' component={Home} />
+            <Route exact path='/home' component={Home} />
+            <Route path='/login' component={Signin} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,88 +1,85 @@
 import React from 'react';
-import {Redirect,Link, BrowserRouter, Route} from 'react-router-dom';
+import { Redirect, Link } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import Home from './Home';
-
-import {Nav, Navbar, NavDropdown, MenuItem, Tabs, ButtonToolbar, 
-        Button, Table, ButtonGroup, Row, Col, Grid, Panel, Form , FormControl} from 'react-bootstrap';
+import { Nav, Navbar, NavDropdown, Form } from 'react-bootstrap';
 
 import './Header.css';
 
 import Signin from './signin';
 
-export class Header extends React.Component{
- constructor(props){
-     super();
-     this.state = {
-         user: props.user
-     }
- }
-  showSinginButton(){
-      console.log(this.state.user);
-      if(this.state.user){
-        return(<div>{this.state.user}</div>);
-    }else{
-        return(<Link to = '/login'>
-          <button className = 'btn btn-primary' >xxxx </button>
-          </Link>);
+export class Header extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      user: props.user
+    }
+  }
+  showSinginButton() {
+    console.log(this.state.user);
+    if (this.state.user) {
+      return (<div>{this.state.user}</div>);
+    } else {
+      return (<Link to='/login'>
+        <button className='btn btn-primary' >xxxx </button>
+      </Link>);
     }
   }
 
-  navSignIn(){
+  navSignIn() {
     console.log("ssssssssss");
-    return(<Redirect to = "/" />);
+    return (<Redirect to="/" />);
   }
 
   routeChange() {
     let path = '/login';
     console.log(path);
-    this.props.history.push(  {Signin} );
+    this.props.history.push({ Signin });
   }
 
-  navbarItems(){
-    if(this.props.user === 'student' ){
-    return(
-      <Nav.Link href="/Courses"> <span className="navItem">Link</span></Nav.Link>
+  navbarItems() {
+    if (this.props.user === 'student') {
+      return (
+        <Nav.Link href="/Courses"> <span className="navItem">Link</span></Nav.Link>
+      );
+    }
+    else {
+
+    }
+  }
+
+  render() {
+    return (
+      <div>
+
+        <Navbar expand="lg" bg="primary" variant="dark">
+          <Navbar.Brand href="/home">
+            <span className="navItem">SLIT</span></Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="/home" > <span className="navItem">Home</span></Nav.Link>
+              <Nav.Link href="/link"> <span className="navItem">Link</span></Nav.Link>
+              {this.navbarItems()}
+              <NavDropdown title="Dropdown" id="nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              </NavDropdown>
+
+            </Nav>
+            <Form inline>
+              <span className="navItem">{this.showSinginButton()}</span>
+            </Form>
+          </Navbar.Collapse>
+        </Navbar>
+        <div>{this.props.children}</div>
+      </div>
     );
-    }
-    else{
-
-    }
   }
-
-  render(){
-  return (
-    <div>
-
-    <Navbar expand="lg" bg="primary" variant="dark">
-    <Navbar.Brand href="/home">
-    <span className="navItem">SLIT</span></Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="mr-auto">
-        <Nav.Link href="/home" > <span className="navItem">Home</span></Nav.Link>
-        <Nav.Link href="/link"> <span className="navItem">Link</span></Nav.Link>
-        {this.navbarItems()}
-        <NavDropdown title="Dropdown" id="nav-dropdown">
-          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-          <NavDropdown.Divider />
-          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-        </NavDropdown>
-        
-      </Nav>
-      <Form inline>
-      <span className="navItem">{this.showSinginButton()}</span>
-    </Form>
-    </Navbar.Collapse>
-  </Navbar>
-  <div>{this.props.children}</div>
-  </div>
-  );
-}
 }
 
 export default Header;
