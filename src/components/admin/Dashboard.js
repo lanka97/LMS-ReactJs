b8e0c14dba71b4860df0fec9d15bab09e6abe06b
Pasan Godamune <pasanjg@gmail.com>
2019-06-14 03:04:14 +0530

    ui changes

--- src/components/admin/Dashboard.js
+++ src/components/admin/Dashboard.js
@@ -22,7 +22,7 @@ export default class Dashboard extends Component {
             labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
             datasets: [{
                 label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
+                data: [12, 19, 10, 5, 7, 3],
                 backgroundColor: [
                     'rgba(255, 99, 132, 0.2)',
                     'rgba(54, 162, 235, 0.2)',
@@ -61,134 +61,134 @@ export default class Dashboard extends Component {
 
         return (
 
-            // <Container fluid={1}>
-            //     <Row>
-            //         <SideBar />
-            <Col style={{ padding: 0 }}>
-                <Container style={{ padding: '20px' }}>
-                    {/* <h2>Dashboard</h2> */}
-                    <Row style={{ color: 'white', textAlign: 'right', marginTop: '30px' }}>
-                        <Col md={4}>
-                            <Card bg="info" body>
-                                <h1>50</h1>
-                                <h4>Students</h4>
-                            </Card>
-                        </Col>
+            <Container fluid={1} style={{fontSize: '14px'}}>
+                <Row>
+                    <SideBar />
+                    <Col style={{ padding: 0 }}>
+                        <Container style={{ padding: '20px' }}>
+                            {/* <h2>Dashboard</h2> */}
+                            <Row style={{ color: 'white', textAlign: 'right', marginTop: '30px' }}>
+                                <Col md={4}>
+                                    <Card bg="info" body>
+                                        <h1>50</h1>
+                                        <h4>Students</h4>
+                                    </Card>
+                                </Col>
 
-                        <Col md={4}>
-                            <Card bg="secondary" body>
-                                <h1>10</h1>
-                                <h4>Instructors</h4>
-                            </Card>
-                        </Col>
+                                <Col md={4}>
+                                    <Card bg="secondary" body>
+                                        <h1>10</h1>
+                                        <h4>Instructors</h4>
+                                    </Card>
+                                </Col>
 
-                        <Col md={4}>
-                            <Card bg="danger" body>
-                                <h1>2</h1>
-                                <h4>Admins</h4>
-                            </Card>
-                        </Col>
-                    </Row>
+                                <Col md={4}>
+                                    <Card bg="danger" body>
+                                        <h1>2</h1>
+                                        <h4>Admins</h4>
+                                    </Card>
+                                </Col>
+                            </Row>
 
-                    <Row style={{ marginTop: '50px' }}>
-                        <Col md={6}>
-                            <Card>
-                                <Card.Header>Latest 10 Students</Card.Header>
-                                <Card.Text style={{ padding: '10px' }}>
-                                    <Table striped bordered hover>
-                                        <tr className="bg-dark text-white">
-                                            <th>#</th>
-                                            <th>Name</th>
-                                            <th>Joined</th>
-                                            <th>Faculty</th>
-                                            <th>Verified</th>
-                                        </tr>
-                                        <tr>
-                                            <th>1</th>
-                                            <td>John Doe</td>
-                                            <td>2019/06/06</td>
-                                            <td>Computing</td>
-                                            <td>Yes</td>
-                                        </tr>
-                                        <tr>
-                                            <th>2</th>
-                                            <td>John Doe</td>
-                                            <td>2019/06/06</td>
-                                            <td>Computing</td>
-                                            <td>Yes</td>
-                                        </tr>
-                                    </Table>
-                                </Card.Text>
-                            </Card>
-                        </Col>
+                            <Row style={{ marginTop: '50px' }}>
+                                <Col md={6}>
+                                    <Card>
+                                        <Card.Header>Latest 10 Students</Card.Header>
+                                        <Card.Text style={{ padding: '10px' }}>
+                                            <Table striped bordered hover size="sm">
+                                                <tr className="bg-dark text-white">
+                                                    <th>#</th>
+                                                    <th>Name</th>
+                                                    <th>Joined</th>
+                                                    <th>Faculty</th>
+                                                    <th>Verified</th>
+                                                </tr>
+                                                <tr>
+                                                    <th>1</th>
+                                                    <td>John Doe</td>
+                                                    <td>2019/06/06</td>
+                                                    <td>Computing</td>
+                                                    <td>Yes</td>
+                                                </tr>
+                                                <tr>
+                                                    <th>2</th>
+                                                    <td>John Doe</td>
+                                                    <td>2019/06/06</td>
+                                                    <td>Computing</td>
+                                                    <td>Yes</td>
+                                                </tr>
+                                            </Table>
+                                        </Card.Text>
+                                    </Card>
+                                </Col>
 
-                        <Col md={6}>
-                            <Card>
-                                <Card.Header>Latest 10 Instructors</Card.Header>
-                                <Card.Text style={{ padding: '10px' }}>
-                                    <Table striped bordered hover>
-                                        <tr className="bg-dark text-white">
-                                            <th>#</th>
-                                            <th>Name</th>
-                                            <th>Joined</th>
-                                            <th>Faculty</th>
-                                            <th>Verified</th>
-                                        </tr>
-                                        <tr>
-                                            <th>1</th>
-                                            <td>John Doe</td>
-                                            <td>2019/06/06</td>
-                                            <td>Computing</td>
-                                            <td>Yes</td>
-                                        </tr>
-                                        <tr>
-                                            <th>2</th>
-                                            <td>John Doe</td>
-                                            <td>2019/06/06</td>
-                                            <td>Computing</td>
-                                            <td>Yes</td>
-                                        </tr>
-                                    </Table>
-                                </Card.Text>
-                            </Card>
-                        </Col>
-                    </Row>
+                                <Col md={6}>
+                                    <Card>
+                                        <Card.Header>Latest 10 Instructors</Card.Header>
+                                        <Card.Text style={{ padding: '10px' }}>
+                                            <Table striped bordered hover size="sm">
+                                                <tr className="bg-dark text-white">
+                                                    <th>#</th>
+                                                    <th>Name</th>
+                                                    <th>Joined</th>
+                                                    <th>Faculty</th>
+                                                    <th>Verified</th>
+                                                </tr>
+                                                <tr>
+                                                    <th>1</th>
+                                                    <td>John Doe</td>
+                                                    <td>2019/06/06</td>
+                                                    <td>Computing</td>
+                                                    <td>Yes</td>
+                                                </tr>
+                                                <tr>
+                                                    <th>2</th>
+                                                    <td>John Doe</td>
+                                                    <td>2019/06/06</td>
+                                                    <td>Computing</td>
+                                                    <td>Yes</td>
+                                                </tr>
+                                            </Table>
+                                        </Card.Text>
+                                    </Card>
+                                </Col>
+                            </Row>
 
-                    <Row style={{ marginTop: '50px' }}>
-                        <Col md={6}>
-                            <Card>
-                                <Card.Header>Some chart</Card.Header>
-                                <Card.Text>
-                                    <Bar
-                                        data={data}
-                                        width={100}
-                                        height={50}
-                                        options={options}
-                                    />
-                                </Card.Text>
-                            </Card>
-                        </Col>
+                            <Row style={{ marginTop: '50px' }}>
+                                <Col md={6}>
+                                    <Card>
+                                        <Card.Header>Some chart</Card.Header>
+                                        <Card.Text>
+                                            <Bar
+                                                data={data}
+                                                width={100}
+                                                height={50}
+                                                options={options}
+                                            />
+                                        </Card.Text>
+                                    </Card>
+                                </Col>
 
-                        <Col md={6}>
-                            <Card>
-                                <Card.Header>Another chart</Card.Header>
-                                <Card.Text>
-                                    <Line
-                                        data={data}
-                                        width={100}
-                                        height={50}
-                                        options={options}
-                                    />
-                                </Card.Text>
-                            </Card>
-                        </Col>
-                    </Row>
+                                <Col md={6}>
+                                    <Card>
+                                        <Card.Header>Another chart</Card.Header>
+                                        <Card.Text>
+                                            <Pie
+                                                data={data}
+                                                width={100}
+                                                height={50}
+                                                options={options}
+                                            />
+                                        </Card.Text>
+                                    </Card>
+                                </Col>
+                            </Row>
 
 
-                </Container>
-            </Col>
-            //      </Row>
-            // </Container> 
+                        </Container>
+                    </Col>
+                </Row>
+            </Container>
 
         )
     }
--- src/components/admin/SideBar.js
+++ src/components/admin/SideBar.js
@@ -2,29 +2,25 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import { Container, Row, Col, Nav, NavDropdown, Form } from 'react-bootstrap';
+import { ListGroup, ListGroupItem } from 'reactstrap';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faUser } from '@fortawesome/free-solid-svg-icons';
 
 
 export default class SideBar extends Component {
     render() {
         return (
-            <Col md={2} style={{ padding: 0 }}>
-                <table className="table ">
-                    <tr>
-                        <td><Nav.Link to="/home"><span className="fa fa-home"></span> Dashboard</Nav.Link></td>
-                    </tr>
-                    <tr>
-                        <td><Nav.Link to="/home"><span className="fa fa-user"></span> Users</Nav.Link></td>
-                    </tr>
-                    <tr>
-                        <td>three</td>
-                    </tr>
-                    <tr>
-                        <td>four</td>
-                    </tr>
-                    <tr>
-                        <td>five</td>
-                    </tr>
-                </table>
+            <Col md={2} style={{ padding: 0}} className="bg-dark">
+                <ListGroup>
+                    <ListGroupItem className="bg-dark text-white"></ListGroupItem>
+                    <ListGroupItem className="bg-dark text-white" tag="a" href="/admin/dashboard" action>
+                        <FontAwesomeIcon icon={faHome} /> &nbsp; Dashboard
+                    </ListGroupItem>
+                    <ListGroupItem className="bg-dark text-white" tag="a" href="/admin/users" action>
+                        <FontAwesomeIcon icon={faUser} /> &nbsp; Users
+                    </ListGroupItem>
+                </ListGroup>
             </Col>
 
         )
