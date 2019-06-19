299869ecf8c15d46c7a296c7d74ce0369b896d70
ShalikaAshan01 <shalikaashan01@gmail.com>
2019-06-19 13:36:29 +0530

    bug fix

---
+++ src/components/functions/submission.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+var headers = {
+    headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+    }
+};
+export const fn_addSubmission = async payload =>{
+
+    return await axios.post(' http://localhost:8080/lms/submission',payload,headers)
+};
+/**
+ * payload JSON must have courseID,assignment,studentID
+ * @param payload
+ * @returns {Promise<AxiosResponse<T>>}
+ */
+export const fn_getSpecificSubmissionInfo = async payload =>{
+    return await axios.get('http://localhost:8080/lms/submission/getassignment',{params:payload})
+};
+
+export const fn_deleteSubmission = async id =>{
+    return await axios.delete("http://localhost:8080/lms/submission/"+id)
+};
+
+export const fn_getAllSubmissionByCourseAndAssignment = async (courseID,assignmentName)=>{
+    return await axios.get(`http://localhost:8080/lms/submission/course/${courseID}/assignment/${assignmentName}`)
+};
+
+export const fn_updateAssignmentMarks = async (submissionID,data) =>{
+    return await axios.put(`http://localhost:8080/lms/submission/${submissionID}`,data,headers);
+};
---
+++ src/components/submission/upload.jsx
@@ -0,0 +1,208 @@
+import React,{Component} from 'react'
+import path from 'path';
+import '../../CSS/upload_assignment.css';
+import {fn_addSubmission, fn_deleteSubmission} from '../functions/submission'
+import moment from "moment";
+import { css } from '@emotion/core';
+import {PropagateLoader} from 'react-spinners';
+import swal from 'sweetalert';
+
+const override = css`
+    display: block;
+    margin: 0 auto;
+    border-color: red;
+`;
+
+class upload extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            background:process.env.PUBLIC_URL + "/images/icons/default-file.png",
+            title:"Upload Assignment",
+            file:File,
+            courseID:"",
+            assignmentName:"",
+            studentID:"",
+            dueDate:"",
+            loading: false,
+            button:"Save",
+            reupload:false
+        };
+        this.getInput =  this.getInput.bind(this);
+    }
+
+    componentDidMount() {
+        let params = this.props.match.params;
+        if(this.props.history.action !== "PUSH" ){
+            window.location.href="/course/"+params.courseID+"/assignment/"+params.assignment+"/status";
+        }
+        let dueDate = this.props.location.state.dueDate;
+
+        this.setState({
+            courseID:params.courseID,
+            assignmentName:params.assignment,
+            studentID:"IT17102674",
+            dueDate:dueDate,
+            reupload:this.props.location.state.reupload,
+            title:this.props.location.state.reupload ? "Edit Submission" : "Upload Assignment"
+
+        });
+
+        let end = moment(dueDate,'DD-MM-YYYY');
+        let startTime = moment();
+
+        if(moment.duration(end.diff(startTime))<=0){
+            window.location.href="/course/"+params.courseID+"/assignment/"+params.assignment+"/status";
+        }
+
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+    getInput(e){
+        let file = e.target.value;
+
+        let extension = path.extname(file).slice(1);
+
+        if(extension==="docx")
+            extension = "doc";
+
+        if(!(extension === "doc" || extension === "xlxs" || extension === "pdf" ||
+            extension === "pptx" || extension === "rar" || extension === "zip")){
+            extension = "unknown"
+        }
+
+        this.setState({
+            background: process.env.PUBLIC_URL + "/images/icons/" + extension +"-file.png",
+            file:e.target.files[0]
+        })
+    }
+
+    onSubmit(){
+
+        let payload = new FormData();
+        payload.set('courseID',this.state.courseID);
+        payload.set('assignmentName',this.state.assignmentName);
+        payload.set('studentID',this.state.studentID);
+        payload.append('file',this.state.file);
+
+        this.setState({
+            title: "Uploading...",
+            loading: true,
+            button: "Saving..."
+        });
+
+        if(this.state.reupload){
+
+
+
+            fn_deleteSubmission(this.props.location.state.uploadID).then(data=>{
+
+                fn_addSubmission(payload).then(data => {
+                    this.props.history.push({
+                        pathname: "/course/" + this.state.courseID + "/assignment/" + this.state.assignmentName + "/status",
+                        state: {upload: true}
+                    })
+
+                }).catch(err => {
+                    this.setState({
+                        title: "Upload Assignment",
+                        loading: false,
+                        button: "Save"
+                    });
+                    console.log(err);
+                    swal({
+                        title: "Internal error has occurred",
+                        text: "Please try again",
+                        icon: "error",
+                        button: "Okay",
+                    });
+                });
+            }).catch(err=>{
+                this.setState({
+                    title: "Upload Assignment",
+                    loading: false,
+                    button: "Save"
+                });
+                console.log(err);
+                swal({
+                    title: "Internal error has occurred",
+                    text: "Please try again",
+                    icon: "error",
+                    button: "Okay",
+                });
+
+            })
+
+        }else {
+            fn_addSubmission(payload).then(data => {
+
+                this.props.history.push({
+                    pathname: "/course/" + this.state.courseID + "/assignment/" + this.state.assignmentName + "/status",
+                    state: {upload: true}
+                })
+
+            }).catch(err => {
+
+                this.setState({
+                    title: "Upload Assignment",
+                    loading: false,
+                    button: "Save"
+                });
+
+                console.log(err);
+                swal({
+                    title: "Internal error has occurred",
+                    text: "Please try again",
+                    icon: "error",
+                    button: "Okay",
+                });
+            });
+        }
+    }
+
+
+    render() {
+        let style = ".files::after {background-image: url("+this.state.background+");}";
+        return(
+            <div className="container">
+
+                <div className="row">
+                    <div className="col-md-12">
+                        <form method="post" action="#" id="#">
+                            <div className="form-group files color mt-5 pt-3">
+                                <div className="h4">{this.state.title}</div>
+                                <input type="file" id="upload" className="form-control mt-5" multiple="" onChange={this.getInput}/>
+                            </div>
+                            <style>{style}
+                            </style>
+
+                            <div className="row">
+                                <div className="col-6"></div>
+                                <div className="col-6">
+                                    <PropagateLoader
+                                    css={override}
+                                    sizeUnit={"px"}
+                                    size={15}
+                                    color={'#123abc'}
+                                    loading={this.state.loading}/></div>
+                            </div>
+
+                            <div className="z-depth-1 mt-2">
+                                <button type="button" className="btn btn-success float-right m-2" onClick={this.onSubmit}>{this.state.button}</button>
+                                <button type="button" className="btn btn-danger float-right m-2"
+                                onClick={this.props.history.goBack}
+                                >Cancel</button>
+                            </div>
+
+                        </form>
+
+
+                    </div>
+                </div>
+            </div>
+
+
+        );
+    }
+}
+export default upload;
